55a20a89aed4968de8e9ffbdf907968a6edc8323
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-10-29 14:31:35 +0900

    [code refactor]매개변수 네이밍

--- src/mock/get-data.js
+++ src/mock/get-data.js
@@ -1,8 +1,8 @@
 import mainJSON from "../../__fixtures__/main.json";
 import detailJSON from "../../__fixtures__/detail.json";
 
-export function getData(data) {
-  switch (data) {
+export function getData(target) {
+  switch (target) {
     case "main":
       return mainJSON;
     case "detail":
