0ca526a57440a9231c71c383d8eaefaf1dd470f0
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-10-29 15:21:09 +0900

    [chore]에러처리 수정

--- src/mock/get-data.js
+++ src/mock/get-data.js
@@ -2,12 +2,18 @@ import mainJSON from "../../__fixtures__/main.json";
 import detailJSON from "../../__fixtures__/detail.json";
 
 export function getData(target) {
-  switch (target) {
-    case "main":
-      return mainJSON;
-    case "detail":
-      return detailJSON;
-    default:
-      return console.error("NO DATA");
+  try {
+    switch (target) {
+      case "main":
+        return mainJSON;
+      case "detail":
+        return detailJSON;
+      default:
+        throw new Error(`NO DATA`);
+    }
+  } catch (error) {
+    if (import.meta.env.DEV) {
+      console.error(error);
+    }
   }
 }
