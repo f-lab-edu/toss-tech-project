7d248db860f2c6fee2679230d899dac83a695a95
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-11-01 21:23:54 +0900

    [chore]코드 리팩토링

--- src/mock/index.js
+++ src/mock/index.js
@@ -4,12 +4,10 @@ import detail from "../api/get-detail";
 
 const initMocks = () => {
   try {
-    
       const mockHandler = [main, detail].map(handler => handler());
       const worker = setupWorker(...mockHandler);
       worker.start();
       return true;
-    
   } catch (error) {
     if (import.meta.env.DEV) {
       console.error(`worker error: ${error}`);
