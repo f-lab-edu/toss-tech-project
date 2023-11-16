d9f2b5e8c83c6a6cc6e362f7aec9387b3a315490
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-11-16 21:03:32 +0900

    [chore]오타 수정

--- src/model/__tests__/detail.test.js
+++ src/model/__tests__/detail.test.js
@@ -1,5 +1,5 @@
 import getDetailData from "../detail";
-import detalData from "../../../__fixtures__/detail.json";
+import detailData from "../../../__fixtures__/detail.json";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
@@ -7,7 +7,7 @@ const fixPath = "http://localhost/tossblog/v1";
 
 const server = setupServer(
   rest.get(`${fixPath}/detail/2`, (req, res, ctx) => {
-    return res(ctx.json(detalData[2]));
+    return res(ctx.json(detailData[2]));
   }),
 );
 
@@ -15,7 +15,7 @@ beforeAll(() => server.listen());
 
 test("getDetailData 함수가 잘 동작하여 데이터를 성공적으로 받아오는지 확인", async () => {
   const result = await getDetailData(2);
-  expect(result).toEqual(detalData[2]);
+  expect(result).toEqual(detailData[2]);
 });
 
 afterAll(() => server.close());
