618eaa63974315adf46c09d3261c99664d0cc5a2
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-11-29 20:24:53 +0900

    [chore]리뷰 반영하여 수정

--- src/views/components/header/index.js
+++ src/views/components/header/index.js
@@ -3,21 +3,19 @@ import "./index.css";
 
 const headerTemplate = `
 <header id="header_container">
-  <div id="header_outer_container">
     <div id="header_inner_container">
-      <nav id="header_logo_container">
+      <div id="header_logo_container">
         <a href="/">
           <img src="${HEADER_LOGO}" alt="토스 로고 아이콘" />
         </a>
-      </nav>
+      </div>
     </div>
-  </div>
 </header>
 `;
 
 const headerComponent = () => {
-  const imageLogo = document.getElementById("header"); //Todo: JAE-37 티켓이서 app 레이아웃 구현 예정입니다.
-  imageLogo.innerHTML = headerTemplate;
+  const headerElement = document.getElementById("header"); //Todo: JAE-37 티켓이서 app 레이아웃 구현 예정입니다.
+  headerElement.innerHTML = headerTemplate;
 };
 
 export default headerComponent;
