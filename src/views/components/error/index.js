2f4eb817cf5dc4fd33eb667a419ebb6c3a9c2676
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-12-05 15:44:09 +0900

    [chore]코드 리팩토링

--- src/views/components/error/index.js
+++ src/views/components/error/index.js
@@ -1,11 +1,12 @@
 import "./index.css";
+
 const errorTemplate = `
 <section id="error_container">
-  <div id="error_inner_container">
-    <div id="notfound_container">
-      <h1>페이지를 찾을수 없습니다.</h1>      
+    <div id="error_inner_container">
+        <div id="notfound_container">
+            <h1>페이지를 찾을수 없습니다.</h1>      
+        </div>
     </div>
-  </div>
 </section>
 `;
 
