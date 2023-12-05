b4051f72d10ba220fe8729099bd37e5335bbc57c
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-12-05 15:43:26 +0900

    [chore]코드 리팩토링

--- src/views/components/title/index.js
+++ src/views/components/title/index.js
@@ -2,11 +2,11 @@ import "./index.css";
 
 const titleTemplate = `
 <section id="title_container">
-  <div id="title_inner_container">
-      <div id="development_container">
-        <h1>개발</h1>      
-      </div>
-  </div>
+    <div id="title_inner_container">
+        <div id="development_container">
+            <h1>개발</h1>      
+        </div>
+    </div>
 </section>
 `;
 
