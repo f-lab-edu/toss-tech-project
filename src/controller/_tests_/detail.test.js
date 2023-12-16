02205e03f7d0481ef40619297597df45d6658399
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2023-12-16 11:25:40 +0900

    [chore]주석 제거

--- src/controller/_tests_/detail.test.js
+++ src/controller/_tests_/detail.test.js
@@ -14,7 +14,6 @@ const detailController = new DetailController(mockRouter, mockModel);
 describe("디테일 컨트롤러가 정상적으로 작동하는지 테스트 하기", () => {
   test("디테일 페이지를 불러오는 메서드를 호출할시 모델의 데이터를 잘 받아와 보여지는지 확인", async () => {
     await detailController.detailPage(1);
-    // 모델의 mainList 및 detailContent 메서드가 제대로 호출되었는지 확인
     expect(mockModel.mainList).toHaveBeenCalledTimes(1);
     expect(mockModel.detailContent).toHaveBeenCalledTimes(1);
   });
