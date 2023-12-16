d45c5b836d7f471988308198d19fca6154e1b2e3
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2023-12-16 20:30:20 +0900

    [feat]콘텐트 컴포넌트 구현

---
+++ src/views/components/content/index.js
@@ -0,0 +1,37 @@
+import "./index.css";
+import formatDate from "../list/format-date";
+
+const contentTemplate = (thumbnailImage, title, createdDate, content) => ` 
+<main>
+  <div id="detail_container">
+    <div id="detail_inner_container">
+      <div id="detail_img_container">
+        <img src="${thumbnailImage}" alt="상세 페이지에 들어있는 이미지" />
+      </div>
+      <section>
+        <div id="detail_title_container">
+          <h1>${title}</h1>
+        </div>
+        <div id="detail_created_container">
+          <p>${formatDate(createdDate)}</p>
+        </div>
+        <div id="detail_content_container">
+          <p>${content}</p>
+        </div>
+      <section>
+    </div>
+  </div>
+</main>
+`;
+
+const contentComponet = (articleItem, content) => {
+  const {
+    thumbnail_image: thumbnailImage,
+    title,
+    created_date: createdDate,
+  } = articleItem;
+  const main = document.getElementById("main");
+  main.innerHTML = contentTemplate(thumbnailImage, title, createdDate, content);
+};
+
+export default contentComponet;
