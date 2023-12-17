ac18691de327e3f320f2f0126e442939430dec29
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2023-12-17 18:15:47 +0900

    [test]테스트 코드작성

--- src/controller/_tests_/detail.test.js
+++ src/controller/_tests_/detail.test.js
@@ -1,8 +1,9 @@
 import DetailController from "../detail";
 
 const mockModel = {
-  mainList: jest.fn().mockResolvedValue(["item1", "item2"]),
-  detailContent: jest.fn().mockResolvedValue("detailContent"),
+  articleItem: jest
+    .fn()
+    .mockResolvedValue({ id: "1", title: "Article 1", content: "Content 1" }),
 };
 
 const mockRouter = {
@@ -12,12 +13,11 @@ const mockRouter = {
 const detailController = new DetailController(mockRouter, mockModel);
 
 describe("디테일 컨트롤러가 정상적으로 작동하는지 테스트 하기", () => {
-  test("디테일 페이지를 불러오는 메서드를 호출할시 모델의 데이터를 잘 받아와 보여지는지 확인", async () => {
+  test("디테일 페이지를 불러올는 메서드를 호출할시 모델의 데이터를 잘 받아와 보여지는지 확인", async () => {
     await detailController.detailPage(1);
-    expect(mockModel.mainList).toHaveBeenCalledTimes(1);
-    expect(mockModel.detailContent).toHaveBeenCalledTimes(1);
+    expect(mockModel.articleItem).toHaveBeenCalledTimes(1);
   });
-  test("라우터의 addRoute 메서드가 정상적으로 호출되는지 확인", () => {
+  test("라우터의 addRoute 함수가 정상적으로 호출되는지 확인", () => {
     expect(mockRouter.addRoute).toHaveBeenCalledWith(
       "/detail/(\\w+)",
       expect.any(Function),
