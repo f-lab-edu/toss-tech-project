6c90fcd741795fa97f568cec31ca472c32178648
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2023-12-17 18:18:44 +0900

    [chore]오타 수정

--- src/controller/_tests_/detail.test.js
+++ src/controller/_tests_/detail.test.js
@@ -13,11 +13,11 @@ const mockRouter = {
 const detailController = new DetailController(mockRouter, mockModel);
 
 describe("디테일 컨트롤러가 정상적으로 작동하는지 테스트 하기", () => {
-  test("디테일 페이지를 불러올는 메서드를 호출할시 모델의 데이터를 잘 받아와 보여지는지 확인", async () => {
+  test("디테일 페이지를 불러오는 메서드를 호출할시 모델의 데이터를 잘 받아와 보여지는지 확인", async () => {
     await detailController.detailPage(1);
     expect(mockModel.articleItem).toHaveBeenCalledTimes(1);
   });
-  test("라우터의 addRoute 함수가 정상적으로 호출되는지 확인", () => {
+  test("라우터의 addRoute 메서드가 정상적으로 호출되는지 확인", () => {
     expect(mockRouter.addRoute).toHaveBeenCalledWith(
       "/detail/(\\w+)",
       expect.any(Function),
